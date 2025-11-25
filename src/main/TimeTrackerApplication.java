eafea6ccd173ac9d871c1d67070c04a305765955
Abhishek Yadav <ay9616790@gmail.com>
2025-11-25 15:20:29 +0530

    Add files via upload

---
+++ src/main/TimeTrackerApplication.java
@@ -0,0 +1,28 @@
+package com.timetracker;
+
+import com.timetracker.util.DatabaseManager;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TimeTrackerApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Initialize database
+        DatabaseManager.getInstance().initializeDatabase();
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Scene scene = new Scene(loader.load(), 800, 600);
+        
+        primaryStage.setTitle("TrackSphere");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
